91059161aed41fa6768833e67b6d11a1448a59b5
makabi-bmk <nojiko0101@gmail.com>
2024-03-27 05:45:17 +0900

    socket.ioでの通信を行うようにしてCORS対応した

--- index.ts
+++ index.ts
@@ -1,33 +1,21 @@
-import express from 'express'
-const app: express.Express = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-//CROS対応（というか完全無防備：本番環境ではだめ絶対）
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*")
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-})
-
-app.listen(3001, () => {
-    console.log("Start on port 3001.")
-})
-
-type User = {
-    id: number
-    name: string
-    email: string
-};
-
-const users: User[] = [
-    { id: 1, name: "User1", email: "user1@test.local" },
-    { id: 2, name: "User2", email: "user2@test.local" },
-    { id: 3, name: "User3", email: "user3@test.local" }
-]
-
-//一覧取得
-app.get('/users', (req: express.Request, res: express.Response) => {
-    res.send(JSON.stringify(users))
-})
+const app = express()
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    path: "/socket/",
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+  
+io.on("connection", (socket) => {
+    console.log("connected");
+});
+  
+httpServer.listen(8080, () => {
+    console.log("Chat server listening on port 8080");
+});
