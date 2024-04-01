0ab2f268e11e7bc713325c8d0207057bbe0e4570
makabi-bmk <nojiko0101@gmail.com>
2024-04-01 10:55:41 +0900

    startコマンドが送られた場合のクライアントへの返答処理を作成

--- index.ts
+++ index.ts
@@ -14,13 +14,29 @@ const io = new Server(httpServer, {
   
 io.on("connection", (socket) => {
     console.log("connected");
+    let state: boolean = false;
+    let data = "";
 
     // ゲームの開始準備
     socket.on("start", (data) => {
-        console.log("start");
-        console.log(data);
+        try {
+            console.log("start");
+            console.log(data);
+            sendData(socket.id, "start", true, data);
+        } catch (error) {
+            // エラーだった場合はエラー文をクライアントに返す
+            console.log(error);
+            if (error instanceof Error) {
+                sendData(socket.id, "start", false, error.message);
+            }
+        }
     });
 });
+
+// クライアントにデータを送信する
+function sendData(socketID: string, command: string, state: boolean, data: string | Object) {
+    io.to(socketID).emit(command, {state, data});
+}
   
 httpServer.listen(8080, () => {
     console.log("Chat server listening on port 8080");
